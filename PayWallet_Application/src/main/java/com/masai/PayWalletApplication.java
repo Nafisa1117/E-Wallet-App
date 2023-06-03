0b9d81dcd2cc4db6820364662995fd9aec646948
Nafisa1117 <nafisaparveen9988@gmail.com>
2023-06-03 10:38:40 -0700

    Entity Files are updated

---
+++ PayWallet_Application/src/main/java/com/masai/PayWalletApplication.java
@@ -0,0 +1,13 @@
+package com.masai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayWalletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PayWalletApplication.class, args);
+	}
+
+}
